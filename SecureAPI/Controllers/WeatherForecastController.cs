8dc2649349d5e3b1649a4b4df01dc4ad63c1ab3e
giriparveen <giriparveen@gmail.com>
2020-12-22 16:25:33 +0530

    First Commit

---
+++ SecureAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SecureAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherForecastController : Controller
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        private readonly ILogger<WeatherForecastController> _logger;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            this._logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
